8e602168a58bac3bdecb2bb92026fe174cb33098
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-03-20 16:56:02 -0300

    fix: corrijindo adicionando a notation rest controller advice

--- src/main/java/dev/java10x/EventClean/infraestructure/exception/ControllerException.java
+++ src/main/java/dev/java10x/EventClean/infraestructure/exception/ControllerException.java
@@ -2,9 +2,12 @@ package dev.java10x.EventClean.infraestructure.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@RestControllerAdvice
 public class ControllerException extends RuntimeException {
 
     public ResponseEntity<Map<String, String>> handleDuplicateEventExceptions() {
